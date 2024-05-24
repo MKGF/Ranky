462bdd1682801566a911920f9c8930e9464a7a42
Mikel Garín <mikelgarin@gmail.com>
2024-05-24 17:24:36 +0200

    guild join listener tests

--- src/test/java/com/desierto/Ranky/TestConfig.java
+++ src/test/java/com/desierto/Ranky/TestConfig.java
@@ -1,10 +1,6 @@
 package com.desierto.Ranky;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +9,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.desierto.Ranky.infrastructure")
 @ComponentScan(basePackages = "com.desierto.Ranky.application")
 public class TestConfig {
-
-  @Bean
-  public Validator validator() {
-    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-    return factory.getValidator();
-  }
+  
 }
