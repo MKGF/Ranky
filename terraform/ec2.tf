f3c1d053e529c7e1199466dd251dcef0056b8de1
Mikel Garín <37108139+MKGF@users.noreply.github.com>
2025-07-28 20:19:21 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,14 +13,14 @@ resource "aws_instance" "ranky-ec2" {
               sudo systemctl enable docker
 
               # Autenticarse con ECR
-              aws ecr get-login-password --region eu-west-3 | sudo docker login --username AWS --password-stdin ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-3.amazonaws.com/ranky-repo
+              aws ecr get-login-password --region eu-west-2 | sudo docker login --username AWS --password-stdin ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/ranky-repo
 
 
               # Tirar la imagen desde ECR
-              sudo docker pull ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-3.amazonaws.com/ranky-repo:latest
+              sudo docker pull ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/ranky-repo:latest
 
               # Ejecutar la imagen
-              sudo docker run -d --name ranky-app ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-3.amazonaws.com/ranky-repo:latest
+              sudo docker run -d --name ranky-app ${var.AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/ranky-repo:latest
               EOF
 
   iam_instance_profile = aws_iam_instance_profile.ranky-ec2-profile.name
