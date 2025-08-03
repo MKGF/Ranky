3233146fa86cdf5028c05b01e204d259553153c7
Mikel Garín <mikelgarin@gmail.com>
2025-08-03 21:31:39 +0200

    Introduced client secret

--- src/main/java/com/desierto/ranky/infrastructure/configuration/DiscordClientConfig.java
+++ src/main/java/com/desierto/ranky/infrastructure/configuration/DiscordClientConfig.java
@@ -4,10 +4,12 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DiscordClientConfig implements RequestInterceptor {
 
   private final ConfigLoader config;
@@ -24,5 +26,6 @@ public class DiscordClientConfig implements RequestInterceptor {
     String encoded = Base64.getEncoder()
         .encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
     template.header("Authorization", "Basic " + encoded);
+    log.info("Request intercepted to add following credentials: " + credentials);
   }
 }
