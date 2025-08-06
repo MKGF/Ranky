38357ffc287fbd0479adb21420bc72a50890bcd7
Mikel Garín <mikelgarin@gmail.com>
2025-08-06 09:17:01 +0200

    Added endpoint to get user details

---
+++ src/main/java/com/desierto/ranky/infrastructure/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.desierto.ranky.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://ranky.top") // Tu dominio frontend
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowCredentials(true) // MUY IMPORTANTE para cookies
+            .allowedHeaders("*");
+      }
+    };
+  }
+}
+
