e535b972166d10f1c34da4dab56a3e2980a3be50
Mikel Garín <mikelgarin@gmail.com>
2025-08-06 09:17:16 +0200

    Added endpoint to get user details

--- src/main/java/com/desierto/ranky/infrastructure/configuration/CorsConfig.java
+++ src/main/java/com/desierto/ranky/infrastructure/configuration/CorsConfig.java
@@ -14,9 +14,9 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://ranky.top") // Tu dominio frontend
+            .allowedOrigins("https://ranky.top")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowCredentials(true) // MUY IMPORTANTE para cookies
+            .allowCredentials(true)
             .allowedHeaders("*");
       }
     };
