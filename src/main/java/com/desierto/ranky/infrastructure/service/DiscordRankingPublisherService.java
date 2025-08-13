662ccb6d8f50b1994971ee714d22df74ac038b3b
Mikel Garín <mikelgarin@gmail.com>
2025-08-13 17:43:33 +0200

    Changed format of publish URL in the hook message

--- src/main/java/com/desierto/ranky/infrastructure/service/DiscordRankingPublisherService.java
+++ src/main/java/com/desierto/ranky/infrastructure/service/DiscordRankingPublisherService.java
@@ -35,8 +35,8 @@ public class DiscordRankingPublisherService {
       if (event.isFromGuild()) {
         String rankingName = discordOptionRetriever.fromEventGetObjectName(event);
         rankingPublisherService.publish(rankingName, event.getGuild());
-        String url = String.format("%s/rankings/fromGuild/%s/ranking/%s", config.getRankyHomeUrl(),
-            event.getGuild().getId(), rankingName);
+        String url = String.format("%srankings/fromGuild/%s/ranking/%s", config.getRankyHomeUrl(),
+            event.getGuild().getId(), rankingName.replace(" ", "%20"));
         event.getHook().sendMessage(
             String.format("Ranking published successfully! You can access it in %s", url)).queue();
       } else {
